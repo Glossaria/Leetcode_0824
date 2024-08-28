fadc985f8b193c64bc32e09912bb2f1cc45ebf0e
Glossaria <leetcode1024@gmail.com>
2024-08-27 21:06:02 -0500

    Add 151

---
+++ src/Array_and_String/Solution_151.java
@@ -0,0 +1,19 @@
+package Array_and_String;
+
+public class Solution_151 {
+     public String reverseWords(String s) {
+            String[] array = s.split(" ");
+            int len = array.length;
+
+            StringBuilder res = new StringBuilder();
+
+            for(int i = len - 1; i >= 0; i--){
+                if(array[i] != ""){
+                    res.append(array[i]);
+                    res.append(" ");
+                }
+            }
+            res.setLength(Math.max(res.length() - 1, 0));
+            return res.toString();
+     }
+}
